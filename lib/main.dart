736283abb669f429d08c559e343ad5763f72a014
Juan Carlos Guerrero <ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCdNsutmtQ9nwDfxB1XNjfL6vDU2rZR1leskDHxjaJcQgdZpUfAO2c9vyrJocVGi1dyNMr+K1acWj3qhBquY3e8wEMiryAUB2AKz+NdB7CuNQf+ZQUuSf37BU/QYVPRDMHwBn0YpyOPgQe1U+a5XNw8u4avIZgXpKdrP9XA5K7JEobuPkjtg1/1P2/01mZRcKqN2fp4lw89bmKHkHXm0opZPbwsCUxCLWPmArDs/xjb4RHI81Era2Su55YOmFGJZ6uLFoBfDHqsMK150bPkicazBEbAH4zWZcHfBl0LF9EbrPAEMPmUfjO1tvyfRqaBFsMlunQhUKGXOZ1OY98ZlaAEiA+KZ5/Sb0GkRd47dtZ8nhfDZGWLuXEbUpyI241UOXb4lPNzsPteL5q5QdZXl1bpxELgDHjASoZAFu36feQ4mGNyfEeW6OlqnGY6vQW7vycJbbALl0ZvgULTYPiRAm8Ywsit8Z53+obRCQgqIHDGnzF9DPrFqyw3ticz0ahv9P2lGkrauinQukMwiikkvNt+/rGpUERnqvXVMrYlGL1+Qe9W40wD3v1tP0NTSoPd33bNFOulhCxI7Vh5rcQ4AY5GTlU8UOjsg0rvSX4GqevUu/3XLk7JhkXLLwW4DL0aqfhi6vgCroOEtkGqGlmvSMRGNeM/+mmudw4q/YmOjS357w== juancarlos.guerrero-ext@dominion-global.com>
2020-05-22 10:31:02 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:preferenciasusuario/src/pages/home_page.dart';
+import 'package:preferenciasusuario/src/pages/settings_page.dart';
+import 'package:preferenciasusuario/src/shared_prefs/preferencias_usuario.dart';
+ 
+void main() async {
+  final prefs = new PreferenciasUsuario();
+  await prefs.initPrefs();
+  runApp(MyApp()); 
+}
+ 
+class MyApp extends StatelessWidget {
+
+  final prefs = new PreferenciasUsuario();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Preferencias',
+      initialRoute: prefs.ultimaPagina,
+      routes: {
+        HomePage.routeName : (BuildContext context) => HomePage(),
+        SettingsPage.routeName : (BuildContext context) => SettingsPage()
+      },
+    );
+  }
+}
+
---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:preferenciasusuario/src/shared_prefs/preferencias_usuario.dart';
+import 'package:preferenciasusuario/src/widgets/menu_widget.dart';
+
+class HomePage extends StatelessWidget {
+  static final String routeName = 'home';
+  final prefs = new PreferenciasUsuario();
+
+  @override
+  Widget build(BuildContext context) {
+
+    prefs.ultimaPagina = HomePage.routeName;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Preferencias de Usuario'),
+        backgroundColor: (prefs.colorSecundario) ? Colors.teal : Colors.blue,
+      ),
+      drawer: MenuWidget(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('Color secundario: ${prefs.colorSecundario}'),
+          Divider(),
+          Text('Género: ${prefs.genero}'),
+          Divider(),
+          Text('Nombre usuario: ${prefs.nombreUsuario}'),
+          Divider()
+        ],
+      ),
+    );
+  }
+}
